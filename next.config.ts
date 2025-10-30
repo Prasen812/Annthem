c3e2e86484a83c013944ffb988bee30dbf131f71
Prasen Shinde <prasenshinde8@gmail.com>
2025-10-30 05:37:31 +0000

    fetch to failed , robust this fetching error and fix it check where it i

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,14 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:5001/api/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
